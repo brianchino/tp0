00f77fcf5e0f0ed580069737d4d8dad41a757e60
brianchino <bchinoyanarico@frba.utn.edu.ar>
2025-08-22 18:13:10 +0000

    ETAPA 3 hecho

--- client/src/client.c
+++ client/src/client.c
@@ -45,7 +45,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
-
+	enviar_mensaje(valor,conexion);
 	// Armamos y enviamos el paquete
 	paquete(conexion);
 
@@ -98,13 +98,23 @@ void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
 	char* leido;
-	t_paquete* paquete;
+	t_paquete* paquete = crear_paquete();;
 
 	// Leemos y esta vez agregamos las lineas al paquete
-
-
-	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
 	
+	leido = readline("> ");
+	
+	
+	while(leido[0] != '\0'){
+		agregar_a_paquete(paquete,leido,strlen(leido) + 1);
+		free(leido);
+		leido = readline("> ");
+		
+	}
+	enviar_paquete(paquete,conexion);
+	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	free(leido);
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
@@ -113,4 +123,5 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 	  log_destroy(logger);
 	  config_destroy(config);
+	  liberar_conexion(conexion);
 }
